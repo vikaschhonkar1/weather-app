ab54a8b57f3eb3a2615573e16252508e8a9c2fb2
Vikas Chhonkar <vikaschhonkar1@gmail.com>
2023-09-01 08:14:15 +0530

    Completed the react weather app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,73 @@
-import './App.css';
+import "./App.css";
+import Inputs from "./components/Inputs";
+import TimeAndLocation from "./components/TimeAndLocation";
+import TemperatureAndDetails from "./components/TemperatureAndDetails";
+import getFormattedWeatherData from "./services/weatherService";
+import { useEffect, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
+  const [query, setQuery] = useState({ q: "delhi" });
+  const [units, setUnits] = useState("metric");
+  const [weather, setWeather] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchWeather = async () => {
+      const message = query.q ? query.q : "current location.";
+
+      toast.info("Fetching weather for " + message);
+
+      try {
+        const data = await getFormattedWeatherData({ ...query, units });
+        toast.success(
+          `Successfully fetched weather for ${data.name}, ${data.country}.`
+        );
+
+        setWeather(data);
+        setError(null);
+      } catch (error) {
+        if (error.response && error.response.status === 404) {
+          setError("City not found. Please enter a valid city name.");
+        } else {
+          setError("An error occurred while fetching weather data.");
+        }
+        setWeather(null);
+      }
+    };
+
+    fetchWeather();
+  }, [query, units]);
+
+  const formatBackground = () => {
+    if (!weather) return "from-cyan-700 to-blue-700";
+    const threshold = units === "metric" ? 20 : 60;
+    if (weather.temp <= threshold) return "from-cyan-700 to-blue-700";
+
+    return "from-yellow-700 to-orange-700";
+  };
+
   return (
-    <>Weather App</>
+    <div
+      className={`mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br  h-fit shadow-xl shadow-gray-400 ${formatBackground()}`}
+    >
+      {/* <TopButtons setQuery={setQuery} /> */}
+      <Inputs setQuery={setQuery} units={units} setUnits={setUnits} />
+
+      {error ? (
+        <div className="error-message">{error}</div>
+      ) : weather ? (
+        <div>
+          <TimeAndLocation weather={weather} />
+          <TemperatureAndDetails weather={weather} />
+        </div>
+      ) : (
+        <div className="loading-message">Loading...</div>
+      )}
+
+      <ToastContainer autoClose={5000} theme="colored" newestOnTop={true} />
+    </div>
   );
 }
 
